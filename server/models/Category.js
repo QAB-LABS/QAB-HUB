51d1e8f3180c253bd9e36901495f074718a2b284
Qiwei Lin <Qiwei.l.ql@gmail.com>
2019-10-11 16:09:08 -0400

    Updated Category model and added routes

--- server/models/Category.js
+++ server/models/Category.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema
 
 const categorySchema = new Schema({
     name: {
-        type: String
+        type: String,
+        required: true
     },
 }, {
     timestamps: {

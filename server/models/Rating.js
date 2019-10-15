c3dbf35bde5caeb06840118250dca90df17cedfc
Qiwei Lin <Qiwei.l.ql@gmail.com>
2019-10-15 17:12:23 -0400

    Created Rating Model

--- server/models/Rating.js
+++ server/models/Rating.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const ratingSchema = new mongoose.Schema({
-  value: Number,
+  value: {type: Number, min: 0, max: 100},
   game: { type: mongoose.Schema.Types.ObjectId, ref: "Game" },
   author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 }, {
@@ -11,6 +11,7 @@ const ratingSchema = new mongoose.Schema({
   }
 })
 
+ratingSchema.index({ game: 1, author: 1 }, { unique: true })
 const Rating = mongoose.model('Rating', ratingSchema)
 
 module.exports = Rating

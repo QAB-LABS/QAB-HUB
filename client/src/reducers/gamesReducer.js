895ba02250fca39b9af6ff069895b902248c44b4
Qiwei Lin <Qiwei.l.ql@gmail.com>
2019-10-17 19:07:11 -0400

    Added reducer for FETCH_GAME

--- client/src/reducers/gamesReducer.js
+++ client/src/reducers/gamesReducer.js
@@ -4,6 +4,8 @@ export default (state = [], action) => {
     switch (action.type) {
         case types.FETCH_GAMES:
             return {...state, games: action.payload }
+        case types.FETCH_GAME:
+            return {...state, games: action.payload }
         case types.CREATE_GAME:
             return {...state, game: action.payload }
         case types.FETCH_GAMES_COUNT:

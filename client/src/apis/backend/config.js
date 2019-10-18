faca21d2f60b2b6076a24f388204b255411e4ca0
Andres Weber <andresmweber@gmail.com>
2019-10-18 08:33:37 -0400

    Trying to fix route to db

--- client/src/apis/backend/config.js
+++ client/src/apis/backend/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 console.log(process.env.API_URL)
 console.log(process.env.TEST)
-const base = process.env.NODE_ENV === 'production' ? '' : `http://${window.location.hostname}:5000`
+const base = process.env.NODE_ENV === 'production' ? process.env.API_URL : `http://${window.location.hostname}:5000`
 
 export default function getService(endpoint) {
     const route = `api/${endpoint}`

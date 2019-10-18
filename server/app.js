d51ea8ae5c2b14f8b34bb85a890731cd05a8ecbb
Andres Weber <andresmweber@gmail.com>
2019-10-18 09:43:34 -0400

    Added this full path to site and cors headers.

--- server/app.js
+++ server/app.js
@@ -31,6 +31,13 @@ app.use(
         credentials: true,
     })
 )
+server.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))

1f903397919f538a0f2108be397442862e0d33f0
Qiwei Lin <Qiwei.l.ql@gmail.com>
2019-10-23 22:16:53 -0400

    Trying another fix

--- server/app.js
+++ server/app.js
@@ -37,7 +37,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.resolve(path.join(__dirname, '../client/build'))))
 
 app.use(
     session({

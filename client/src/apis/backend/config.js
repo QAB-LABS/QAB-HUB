0c75fd655dba56c9e20a4cda9517d090d1d56119
Andres Weber <andresmweber@gmail.com>
2021-04-21 18:57:57 -0400

    Mimicked the setup from https://github.com/mars/heroku-cra-node/tree/master/react-ui

--- client/src/apis/backend/config.js
+++ client/src/apis/backend/config.js
@@ -1,13 +1,15 @@
 import axios from 'axios'
 
-console.log(`NODE_ENV: ${process.env.NODE_ENV}, API_URL: ${process.env.API_URL}`)
-const base = process.env.NODE_ENV === 'production' ? '' : `https://127.0.0.1:${process.env.PORT}` // WAS https://${window.location.hostname}
+console.log(
+  `NODE_ENV: ${process.env.NODE_ENV}, API_URL: ${process.env.API_URL}`
+)
+const base = ''
 
 export default function getService() {
-    const baseURL = base
-    console.log(`Creating service with endpoint: ${baseURL}`)
-    return axios.create({
-        baseURL,
-        withCredentials: true,
-    })
+  const baseURL = base
+  console.log(`Creating service with endpoint: ${baseURL}`)
+  return axios.create({
+    baseURL,
+    withCredentials: true,
+  })
 }

685e74ee524480afe5a39e2b1ea20c967d333444
Andres Weber <andresmweber@gmail.com>
2021-04-21 18:45:06 -0400

    Changed to reference port and localhost.

--- client/src/apis/backend/config.js
+++ client/src/apis/backend/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 console.log(`NODE_ENV: ${process.env.NODE_ENV}, API_URL: ${process.env.API_URL}`)
-const base = process.env.NODE_ENV === 'production' ? '' : `https://${window.location.hostname}:5000`
+const base = process.env.NODE_ENV === 'production' ? '' : `https://127.0.0.1:${process.env.PORT}` // WAS https://${window.location.hostname}
 
 export default function getService() {
     const baseURL = base

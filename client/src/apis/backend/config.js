d302483758933ec8e6354a169e56a65f48a2126d
Andres Weber <andresmweber@gmail.com>
2021-04-21 18:30:16 -0400

    Added https to url for client requests.

--- client/src/apis/backend/config.js
+++ client/src/apis/backend/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 console.log(`NODE_ENV: ${process.env.NODE_ENV}, API_URL: ${process.env.API_URL}`)
-const base = process.env.NODE_ENV === 'production' ? '' : `http://${window.location.hostname}:5000`
+const base = process.env.NODE_ENV === 'production' ? '' : `https://${window.location.hostname}:5000`
 
 export default function getService() {
     const baseURL = base

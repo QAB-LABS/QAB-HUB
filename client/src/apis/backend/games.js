3b6bcd71d83437e4f9b20f2e61bd926b70f5e5da
Andres Weber <andresmweber@gmail.com>
2021-04-22 20:32:00 -0400

    Added proper field population for GET /games/:id

--- client/src/apis/backend/games.js
+++ client/src/apis/backend/games.js
@@ -3,68 +3,69 @@ import getService from './config'
 
 const service = getService('')
 
-
 export default {
-    searchGames(filter, skip, limit, sort, populate, query) {
-        return service
-            .get('/api/games/search' + (query || ''), {
-                params: {
-                    filter: new RegExp(filter, "gi"),
-                    skip,
-                    limit,
-                    sort,
-                    populate
-                }
-            })
-            .then(res => res.data)
-            .catch(errHandler)
-    },
+  searchGames(filter, skip, limit, sort, populate, query) {
+    return service
+      .get('/api/games/search' + (query || ''), {
+        params: {
+          filter: new RegExp(filter, 'gi'),
+          skip,
+          limit,
+          sort,
+          populate,
+        },
+      })
+      .then((res) => res.data)
+      .catch(errHandler)
+  },
 
-    getGames(skip, limit, populate) {
-        return service
-            .get('/api/games', {
-                params: {
-                    skip,
-                    limit,
-                    populate
-                }
-            })
-            .then(res => res.data)
-            .catch(errHandler)
-    },
+  getGames(skip, limit, populate) {
+    return service
+      .get('/api/games', {
+        params: {
+          skip,
+          limit,
+          populate,
+        },
+      })
+      .then((res) => res.data)
+      .catch(errHandler)
+  },
 
-    getGamesCount() {
-        return service
-            .get('/api/games/count')
-            .then(res => res.data)
-            .catch(errHandler)
-    },
+  getGamesCount() {
+    return service
+      .get('/api/games/count')
+      .then((res) => res.data)
+      .catch(errHandler)
+  },
 
-    addGame(body) {
-        return service
-            .post('/api/games', body)
-            .then(res => res.data)
-            .catch(errHandler)
-    },
+  addGame(body) {
+    return service
+      .post('/api/games', body)
+      .then((res) => res.data)
+      .catch(errHandler)
+  },
 
-    getGame(id) {
-        return service
-            .get(`/api/games/${id}`)
-            .then(res => res.data)
-            .catch(errHandler)
-    },
+  getGame(id) {
+    return service
+      .get(`/api/games/${id}`, {
+        params: { populate: 'ratings categories likes' },
+      })
+      .then((res) => res.data)
+      .catch(errHandler)
+  },
 
-    updateGame(id, body) {
-        return service
-            .patch(`/api/games/${id}`, body)
-            .then(res => res.data)
-            .catch(errHandler)
-    },
+  updateGame(id, body) {
+    return service
+      .patch(`/api/games/${id}`, body)
+      .then((res) => res.data)
+      .catch(errHandler)
+  },
 
-    deleteGame(id) {
-        return service
-            .delete(`/api/games/${id}`)
-            .then(res => res.data)
-            .catch(errHandler)
-    },
+  deleteGame(id) {
+    return service
+      .delete(`/api/games/${id}`)
+      .then((res) => res.data)
+      .catch(errHandler)
+  },
 }
--- server/routes/games.js
+++ server/routes/games.js
@@ -1,170 +1,182 @@
-const aqp = require('api-query-params');
+const aqp = require('api-query-params')
 const express = require('express')
 const { isLoggedIn } = require('../middlewares')
 const Game = require('../models/Game')
 const router = express.Router()
 
-const populatable_virtuals = 'reviews ratings categories category_names likes';
+const populatable_virtuals = 'reviews ratings categories category_names likes'
 
 const fields = [
-    "url",
-    "name",
-    "description",
-    "price",
-    "image",
-    "year_published",
-    "min_players",
-    "max_players",
-    "min_playtime",
-    "max_playtime",
-    "min_age",
-    "mechanics",
-    "designers",
-    "artists",
-    "publisher",
-    "family",
-    "categories"
+  'url',
+  'name',
+  'description',
+  'price',
+  'image',
+  'year_published',
+  'min_players',
+  'max_players',
+  'min_playtime',
+  'max_playtime',
+  'min_age',
+  'mechanics',
+  'designers',
+  'artists',
+  'publisher',
+  'family',
+  'categories',
 ]
 
 // function sortHelper(a, b){
 //     a.likes > 1
 // }
 
-/** 
- * Get all games with the given search parameters 
+/**
+ * Get all games with the given search parameters
  * Only supports limit currently.
  * @example
  * GET /api/games/search?limit=50
  * */
-router.get('/search', async(req, res, next) => {
-    const { filter, skip, limit, sort, projection, population } = aqp(req.query);
-    console.log(filter, skip, limit, sort, projection, population)
-    Game.find(filter)
-        .lean()
-        .skip(skip)
-        .limit(limit || 50)
-        .sort(sort)
-        .select(projection)
-        .populate(population)
-        .then(games => res.json(games))
-        .catch(err => next(err))
+router.get('/search', async (req, res, next) => {
+  const { filter, skip, limit, sort, projection, population } = aqp(req.query)
+  console.log(filter, skip, limit, sort, projection, population)
+  Game.find(filter)
+    .lean()
+    .skip(skip)
+    .limit(limit || 50)
+    .sort(sort)
+    .select(projection)
+    .populate(population)
+    .then((games) => res.json(games))
+    .catch((err) => next(err))
 })
 
-/** 
- * Get all games.  
+/**
+ * Get all games.
  * @example
  * GET /api/games/
  * */
-router.get('/', async(req, res, next) => {
-    const { skip, limit, population } = aqp(req.query);
-    res.json(await Game
-        .find()
-        .lean()
-        .skip(skip || 0)
-        .limit(limit || 20)
-        .populate(population))
+router.get('/', async (req, res, next) => {
+  const { skip, limit, population } = aqp(req.query)
+  res.json(
+    await Game.find()
+      .lean()
+      .skip(skip || 0)
+      .limit(limit || 20)
+      .populate(population)
+  )
 })
 
-/** 
+/**
  * Get all games and sort by number of likes.
  * @example
  * GET /api/games/by-likes
  * */
-router.get('/by-likes', async(req, res, next) => {
-    const { skip, limit, population } = aqp(req.query);
-    res.json(await Game
-        .find()
-        .lean()
-        .populate(populatable_virtuals))
+router.get('/by-likes', async (req, res, next) => {
+  const { skip, limit, population } = aqp(req.query)
+  res.json(await Game.find().lean().populate(populatable_virtuals))
 })
 
 /**
  * Create a game
- * @example 
+ * @example
  * POST /api/games/
  */
 router.post('/', isLoggedIn, (req, res, next) => {
-    if (req.user.role !== "admin") res.status(403).send('You do not have permission to add a new game.')
-    const postData = {}
-    fields.forEach(field => postData[field] = req.body[field])
+  if (req.user.role !== 'admin')
+    res.status(403).send('You do not have permission to add a new game.')
+  const postData = {}
+  fields.forEach((field) => (postData[field] = req.body[field]))
 
-    Game.create(postData)
-        .then((game) => {
-            res.json(game)
-        }).catch(err => {
-            res.status(404).send(err)
-        })
+  Game.create(postData)
+    .then((game) => {
+      res.json(game)
+    })
+    .catch((err) => {
+      res.status(404).send(err)
+    })
 })
 
 /**
  * Get the total number of games
- * @example 
+ * @example
  * GET /api/games/count
  */
-router.get('/count', async(req, res, next) => {
-    try {
-        res.json({ count: await Game.countDocuments() })
-    } catch (e) {
-        res.status(404).send()
-    }
+router.get('/count', async (req, res, next) => {
+  try {
+    res.json({ count: await Game.countDocuments() })
+  } catch (e) {
+    res.status(404).send()
+  }
 })
 
 /**
- * Get a specific game 
- * @example 
+ * Get a specific game
+ * @example
  * GET /api/games/:id
  */
-router.get('/:id', async(req, res, next) => {
-    try {
-        const game = await Game.findById(req.params.id)
-            .populate(populatable_virtuals)
-        if (!game) throw new Error()
-        res.send(game)
-    } catch (e) {
-        res.status(404).send(e)
-    }
+router.get('/:id', async (req, res, next) => {
+  try {
+    const game = await Game.findById(req.params.id).populate([
+      { path: 'category_names' },
+      { path: 'reviews', populate: { path: 'author' } },
+      { path: 'ratings', populate: { path: 'author' } },
+      { path: 'likes' },
+    ])
+    console.log(game)
+    if (!game) throw new Error()
+    res.send(game)
+  } catch (e) {
+    res.status(404).send(e)
+  }
 })
 
 /**
  * Delete a specific game
- * @example 
+ * @example
  * DELETE /api/games/:id
  */
-router.delete('/:id', isLoggedIn, async(req, res) => {
-    try {
-        const game = await Game.findById(req.params.id)
-        if (!game) throw new Error()
-        if (req.user.role !== "admin") res.status(403).send('You do not have permission to delete this resource.')
-        await game.remove()
-        res.status(202).send(game)
-    } catch (e) {
-        res.status(404).send(e)
-    }
+router.delete('/:id', isLoggedIn, async (req, res) => {
+  try {
+    const game = await Game.findById(req.params.id)
+    if (!game) throw new Error()
+    if (req.user.role !== 'admin')
+      res
+        .status(403)
+        .send('You do not have permission to delete this resource.')
+    await game.remove()
+    res.status(202).send(game)
+  } catch (e) {
+    res.status(404).send(e)
+  }
 })
 
 /**
  * Update a specific game
- * @example 
+ * @example
  * PATCH /api/games/:id
  */
-router.patch(`/:id`, isLoggedIn, async(req, res) => {
-    if (req.user.role !== "admin") res.status(403).send('You do not have permission to update this resource.')
+router.patch(`/:id`, isLoggedIn, async (req, res) => {
+  if (req.user.role !== 'admin')
+    res.status(403).send('You do not have permission to update this resource.')
 
-    const updates = Object.keys(req.body)
-    const allowedUpdates = fields
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+  const updates = Object.keys(req.body)
+  const allowedUpdates = fields
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  )
 
-    if (!isValidOperation) return res.status(400).send({ error: 'Invalid updates!' })
+  if (!isValidOperation)
+    return res.status(400).send({ error: 'Invalid updates!' })
 
-    try {
-        const game = await Game.findById(req.params.id)
-        if (!game) throw new Error()
-        updates.forEach((update) => game[update] = req.body[update])
-        await game.save()
-        res.json(game)
-    } catch (e) {
-        res.status(400).send(e)
-    }
+  try {
+    const game = await Game.findById(req.params.id)
+    if (!game) throw new Error()
+    updates.forEach((update) => (game[update] = req.body[update]))
+    await game.save()
+    res.json(game)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
 module.exports = router
